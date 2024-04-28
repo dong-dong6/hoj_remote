73bd56a7f46840c4afb223bf0c46567aa60b5082
栋dong <dongdong741236@outlook.com>
2024-04-28 21:23:53 +0800

    websocket优化，提供结果获取接口

--- src/main/java/cn/omvn/acwing/WebSocketWithCookies.java
+++ src/main/java/cn/omvn/acwing/WebSocketWithCookies.java
@@ -1,42 +1,45 @@
 package cn.omvn.acwing;
 
+import ch.qos.logback.core.status.StatusListener;
 import cn.omvn.util.ReUtil;
+import lombok.Setter;
 import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
 import java.net.HttpCookie;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class WebSocketWithCookies extends WebSocketClient {
 
     private Map<String, String> cookies;
-    public static String flag;
-    public String code = "{\"activity\":\"problem_submit_code\",\"problem_id\":1,\"code\":\"#include <iostream>\\n#include <cstring>\\n#include <algorithm>\\n\\nusing namespace std;\\n\\nint main()\\n{\\n    int a;\\n    int b;\\n    cin>>a>>b;\\n    cout<<a+b;\\n}\",\"language\":\"C++\",\"mode\":\"normal\",\"problem_activity_id\":0,\"record\":\"[]\",\"program_time\":0}";
-    public WebSocketWithCookies(URI serverUri, Map<String, String> cookies) {
+    public String code;
+    public String result;
+    @Setter
+    private StatusListener statusListener;
+    public WebSocketWithCookies(URI serverUri,String code,Map<String, String> cookies) {
         super(serverUri);
         this.cookies = cookies;
+        this.code = code;
     }
 
     @Override
     public void onOpen(ServerHandshake handshakedata) {
         System.out.println("Opened connection");
-        send("{\n" +
-                "  \"activity\": \"heartbeat\"\n" +
-                "}");
-
+        send(code);
     }
 
     @Override
     public void onMessage(String message) {
         System.out.println("Received: " + message);
         String s = ReUtil.get("status\":\\s*\"(\\w+)", message, 1);
-        System.out.println("当前判题状态" + s);
-
-        flag = s;
-
+        result = s;
+        //System.out.println("当前判题状态" + s);
+        if (statusListener != null) {
+            statusListener.onStatusReceived(s);
+        }
     }
 
     @Override
@@ -54,32 +57,34 @@ public class WebSocketWithCookies extends WebSocketClient {
         if (cookies != null && !cookies.isEmpty()) {
             String cookieHeader = cookies.entrySet().stream()
                     .map(entry -> entry.getKey() + "=" + entry.getValue())
-                    .reduce((c1, c2) -> c1 + "; " + c2)
+                    .reduce((c1, c2) -> c1 + ";" + c2)
                     .orElse("");
+            System.out.println(cookieHeader);
             addHeader("Cookie", cookieHeader);
             addHeader("Origin", "https://www.acwing.com");
         }
-
         super.connect();
     }
 
-    public void judge(String json,WebSocketWithCookies client) {
-        client.connect();
-
+    interface StatusListener {
+        void onStatusReceived(String status);
     }
 
     public static void main(String[] args) {
         HttpCookie csrftoken = cookie.getCsrftoken();
         HttpCookie sessionid = cookie.getSessionid();
-
         Map<String, String> cookieMap = new HashMap<>();
         cookieMap.put("csrftoken", csrftoken.getValue());
         cookieMap.put("sessionid", sessionid.getValue());
         System.out.println(csrftoken.toString());
         System.out.println(sessionid.toString());
-
-        WebSocketWithCookies client = new WebSocketWithCookies(URI.create("wss://www.acwing.com/wss/socket/"), cookieMap);
-        client.judge(client.code,client);
-        System.out.println(flag);
+        String code = "{\"activity\":\"problem_submit_code\",\"problem_id\":1,\"code\":\"#include <iostream>\\n#include <cstring>\\n#include <algorithm>\\n\\nusing namespace std;\\n\\nint main()\\n{\\n    int a;\\n    int b;\\n    cin>>a>>b;\\n    cout<<a+b;\\n}\",\"language\":\"C++\",\"mode\":\"normal\",\"problem_activity_id\":0,\"record\":\"[]\",\"program_time\":0}";
+        WebSocketWithCookies client = new WebSocketWithCookies(URI.create("wss://www.acwing.com/wss/socket/"), code,cookieMap);
+        client.setStatusListener(status -> {
+            // Do something with the status received
+            System.out.println("当前判题状态：" + status);
+            // You can notify other parts of your code here
+        });
+        client.connect();
     }
 }
